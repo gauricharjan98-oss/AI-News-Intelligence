3af716c6c05a7c8441725c6cf7e02f9ed90f9001
gauricharjan98-oss <gauricharjan98@gmail.com>
2026-07-29 15:14:52 +0530

    Delete init_db.py

--- init_db.py
+++
@@ -1,8 +0,0 @@
-from db.database import engine, Base
-from db import models
-
-print("Creating database tables...")
-
-Base.metadata.create_all(bind=engine)
-
-print("Database created successfully!")
